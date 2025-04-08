5433122f3bd54007325803139af7e080fdf434ec
ManishMehra1987 <119958361+ManishMehra1987@users.noreply.github.com>
2025-04-08 23:52:49 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+#Task 1: Perform Basic Mathematical Operations
+
+f = input('Enter the first number: ')
+s = input('Enter the second number: ')
+
+f = int(f)
+s = int(s)
+
+addition = f + s
+subtraction = f - s
+multiplication = f * s
+division = f / s
+
+print('Addition:', addition)
+print('Subtraction:', subtraction)
+print('Multiplication:', multiplication)
+print('Division', division)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+#Task 2: Create a Personalized Greeting
+
+f_name = input('Enter Your First Name: ')
+l_name = input('Enter Your Last Name: ')
+full_name = f_name +' '+l_name
+
+print('Hello,',full_name+'!','Welcome to the Python program.')
